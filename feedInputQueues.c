a968b88e0fa8ecf829a04f55fee4a52e623b7d1e
Nnadozie <okekennadozie@yahoo.com>
2015-12-13 22:52:00 +0000

    Update feedInputQueues.c

--- feedInputQueues.c
+++ feedInputQueues.c
@@ -2,5 +2,5 @@
    Author: Okeke Nnadozie
    Function Name: feedInputQueues
    ******************************************************************************************************
-   Function: feedInputQueues reads the example inputs and feeds them into their appropriate input queues.
+   Function: feedInputQueues reads example inputs and feeds them into their appropriate input queues.
    ****************************************************************************************************** */
