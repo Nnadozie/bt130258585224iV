7f7a5d689b323d0858d02af6fd052a700fa72f0d
Okeke Nnadozie <okekennadozie@yahoo.com>
2015-12-16 07:20:38 +0000

    Minor text move arounds

--- inputQueue.h
+++ inputQueue.h
@@ -21,8 +21,8 @@
     } *front[11], *rear[11], *temp[11], *front2[11];
 //end l2packet
 
-  void enqueue(char srcAdrs, char dstAdrs, char l2PayLoad[], int packDstQ); /* consider how you'll make the argument into a single sturcture argument that can be passed at a go*/
-  void dequeue(int packDstQ);
-  void create(int packDstQ);
-  int queuesize(int packDstQ);
-  void display(int packDstQ);
+    void enqueue(char srcAdrs, char dstAdrs, char l2PayLoad[], int packDstQ);
+    void dequeue(int packDstQ);
+    void create(int packDstQ);
+    int queuesize(int packDstQ);
+    void display(int packDstQ);
